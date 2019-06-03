49e657082396c4e8bfa47de253bbaeaa109ed72d
Dilraj Singh <dilrajkehra@gmail.com>
2019-06-03 09:37:09 -0500

    code fixes

--- HouseholdManager/Models/Home/UserModel.cs
+++ HouseholdManager/Models/Home/UserModel.cs
@@ -1,8 +1,12 @@
-﻿namespace HouseholdManager.Models.Home
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace HouseholdManager.Models.Home
 {
     public class UserModel
     {
         public string UserName { get; set; }
-        public string access_token { get; set; }
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
     }
 }
